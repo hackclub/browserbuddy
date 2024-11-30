48feb3db6ab8b285fe5bc453cbcf174f38c5612d
ooffyy <72337907+realooffyy@users.noreply.github.com>
2024-11-30 23:46:21 +0000

    Extension upload

---
+++ submissions/PMT Buddy/config/webpack.config.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const { merge } = require("webpack-merge");
+
+const common = require("./webpack.common.js");
+const PATHS = require("./paths");
+
+// Merge webpack configuration files
+const config = merge(common, {
+    entry: {
+        popup: PATHS.src + "/js/popup.js",
+        contentScript: PATHS.src + "/js/contentScript.js",
+        background: PATHS.src + "/js/background.js",
+    },
+});
+
+module.exports = config;
