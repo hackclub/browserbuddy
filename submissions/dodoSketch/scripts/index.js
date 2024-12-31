220bcd9aa2d27baa0de6a2b3593eeabf9e528a5a
Lim Xin Ying <m001720@permatapintar.ukm.edu.my>
2024-12-31 13:32:37 +0800

    clear listener

--- submissions/dodoSketch/scripts/index.js
+++ submissions/dodoSketch/scripts/index.js
@@ -1,3 +1,7 @@
 document.getElementById('open').addEventListener('click', () => {
     chrome.runtime.sendMessage({ action: 'createFloatingWindow' });
 });
+
+document.getElementById('clear').addEventListener('click', () => {
+    chrome.runtime.sendMessage({ action: 'clearWindow' });
+});
